7239775f60cf455d3bdbfe14b32cc2aec9b2c317
Bernard Budano <bbudano@gmail.com>
2023-05-13 21:02:00 +0200

    Implemented jwt auth with httpBasic

--- src/main/java/com/example/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
+++ src/main/java/com/example/springsecurityjwtdemo/SpringSecurityJwtDemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.springsecurityjwtdemo;
 
+import com.example.springsecurityjwtdemo.config.RSAKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RSAKeyProperties.class)
 public class SpringSecurityJwtDemoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/springsecurityjwtdemo/config/SecurityConfig.java
@@ -0,0 +1,79 @@
+package com.example.springsecurityjwtdemo.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final RSAKeyProperties rsaKeyProperties;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        var users = List.of(
+                User.withUsername("admin").password("{noop}admin").roles("ADMIN").build(),
+                User.withUsername("user").password("{noop}user").roles("USER").build()
+        );
+
+        return new InMemoryUserDetailsManager(users);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withPublicKey(rsaKeyProperties.publicKey())
+                .build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        var jwk = new RSAKey
+                .Builder(rsaKeyProperties.publicKey())
+                .privateKey(rsaKeyProperties.privateKey())
+                .build();
+
+        var jwkSet = new JWKSet(jwk);
+
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(jwkSet);
+
+        return new NimbusJwtEncoder(jwks);
+    }
+
+}
