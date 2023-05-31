3566601a10c71282de785854aed5d8d3cb5f27eb
Bernard Budano <bbudano@gmail.com>
2023-05-31 23:48:38 +0200

    Token blacklisting WIP

--- src/main/java/com/example/springsecurityjwtdemo/model/BlacklistedToken.java
+++ src/main/java/com/example/springsecurityjwtdemo/model/BlacklistedToken.java
@@ -4,15 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@Table(name = "tokens")
-public class Token {
+@Table(name = "blacklisted_tokens")
+@NoArgsConstructor
+public class BlacklistedToken {
 
     @Id
     private Long id;
 
     private String val;
+
+    public BlacklistedToken(String val) {
+        this.val = val;
+    }
 
 }
