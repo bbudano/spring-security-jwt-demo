f5bd68acf809223900d3cb43b175ed1e65bf02ae
Bernard Budano <bbudano@gmail.com>
2023-06-03 23:46:15 +0200

    Updated jwt filter

--- src/main/java/com/example/springsecurityjwtdemo/config/JwtFilter.java
+++ src/main/java/com/example/springsecurityjwtdemo/config/JwtFilter.java
@@ -6,13 +6,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
@@ -22,15 +24,22 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getServletPath().startsWith("/api/v1/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
-        if (request.getHeader("Authorization") != null) {
-            var authHeader = request.getHeader("Authorization");
+        var authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer")) {
             var token = authHeader.substring(7);
 
             var jwt = jwtDecoder.decode(token);
 
             if (blacklistedTokenService.existsByJti(jwt.getClaim("jti"))) {
-                throw new InvalidBearerTokenException("Invalid token");
+                log.info("Ignoring attempted request with blacklisted token");
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                return;
             }
         }
 
